c514fb4dcf944e230b25b19d526f794220af50a1
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-23 19:20:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,9 +12,8 @@ class EmailAddressParser
   end
   
   def parse
-    #split_emails = email_addresses.split(/[,\s]+/)
-    #split_emails.uniq
-    email_addresses.delete(",").split.uniq
+    split_emails = email_addresses.split(/[,\s]+/)
+    split_emails.uniq
   end
   
 end
